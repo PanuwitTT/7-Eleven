c62708d3afba1cf4ea8406b9bbd8909f8d45d352
Panuwit Khuanpeng <copy.zone.panuwit@gmail.com>
2024-01-16 21:48:47 +0700

    Passed 1st test.

---
+++ tests/seven_eleven/test_7-eleven.py
@@ -0,0 +1,13 @@
+"""Test 7-11"""
+
+#Standard library
+
+#3rd Party library
+import pytest 
+
+#Project library 
+#from seven_eleven.seven from print_7_eleven
+
+def test_print_7_eleven():
+    """Print 7-eleven."""
+    
